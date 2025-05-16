01bacc71e1f971eeeaf26010808036d1b4df874d
Jelang Ramadhan <144755170+JelangR@users.noreply.github.com>
2025-05-16 11:00:53 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -41,6 +41,60 @@ def AUTO_RKM(data):
 
     return rkm, kategori_keluhan
 
+def vis_kecamatan(data):
+    required_cols = {'Kecamatan', 'Instansi','Status'}
+    if not required_cols.issubset(data.columns):
+        raise ValueError(f"Tidak Dapat Melakukan Visualisasi Karena Tidak Terdapat Kolom: {required_cols}")
+
+    data_kecamatan = data[list(required_cols)]
+    selesai = data_kecamatan[data_kecamatan['Status'] == 'Selesai']
+
+    rkm_kecamatan = selesai[selesai['Instansi'].str.startswith('Kecamatan')]
+    rkm_kecamatan = rkm_kecamatan['Instansi'].value_counts().reset_index()
+    rkm_kecamatan.columns = ['Instansi', 'Jumlah']
+
+    rkm_kecamatan = rkm_kecamatan.sort_values(by='Jumlah', ascending=False).head(5)
+
+    # Grafik batang
+    bars_kecamatan = alt.Chart(rkm_kecamatan).mark_bar(
+        cornerRadiusTopLeft=5,
+        cornerRadiusTopRight=5
+    ).encode(
+        x=alt.X('Instansi:N', sort='-y', title='Kecamatan'),
+        y=alt.Y('Jumlah:Q', title='Jumlah Keluhan'),
+        color=alt.Color('Instansi:N', legend=None, scale=alt.Scale(scheme='category20')),
+        tooltip=['Instansi', 'Jumlah']
+    )
+
+    # Label jumlah
+    text_kecamatan = alt.Chart(rkm_kecamatan).mark_text(
+        align='center',
+        baseline='bottom',
+        dy=-5,
+        fontSize=12,
+        color='white'
+    ).encode(
+        x=alt.X('Instansi:N', sort='-y'),
+        y='Jumlah:Q',
+        text='Jumlah:Q'
+    )
+
+    # Gabungkan grafik batang dan label
+    chart_kecamatan = (bars_kecamatan + text_kecamatan).properties(
+        width=700,
+        height=400
+    ).configure_axis(
+        labelFontSize=12,
+        titleFontSize=14
+    ).configure_title(
+        fontSize=18,
+        anchor='start',
+        color='gray'
+    ).configure_axisX(
+        labelLimit=0,
+        labelAngle=45 
+    )
+    st.altair_chart(chart_kecamatan, use_container_width=True)
 
 st.title("AUTO-RKM")
 st.markdown("""
@@ -133,54 +187,10 @@ if uploaded_file is not None:
         )
         st.altair_chart(chart, use_container_width=True)
 
-        # Visualisasi Kecamatan
-        rkm_kecamatan = rkm[rkm['Instansi'].str.startswith('Kecamatan')]
-        if not rkm_kecamatan.empty:
-            rkm_kecamatan = rkm_kecamatan.sort_values(by='Jumlah', ascending=False).head(5)
-            st.subheader("5 Kecamatan dengan Keluhan Masyarakat Terbanyak")
-
-            # Grafik batang
-            bars_kecamatan = alt.Chart(rkm_kecamatan).mark_bar(
-                cornerRadiusTopLeft=5,
-                cornerRadiusTopRight=5
-            ).encode(
-                x=alt.X('Instansi:N', sort='-y', title='Kecamatan'),
-                y=alt.Y('Jumlah:Q', title='Jumlah Keluhan'),
-                color=alt.Color('Instansi:N', legend=None, scale=alt.Scale(scheme='category20')),
-                tooltip=['Instansi', 'Jumlah']
-            )
-
-            # Label jumlah
-            text_kecamatan = alt.Chart(rkm_kecamatan).mark_text(
-                align='center',
-                baseline='bottom',
-                dy=-5,
-                fontSize=12,
-                color='white'
-            ).encode(
-                x=alt.X('Instansi:N', sort='-y'),
-                y='Jumlah:Q',
-                text='Jumlah:Q'
-            )
-
-            # Gabungkan grafik batang dan label
-            chart_kecamatan = (bars_kecamatan + text_kecamatan).properties(
-                width=700,
-                height=400
-            ).configure_axis(
-                labelFontSize=12,
-                titleFontSize=14
-            ).configure_title(
-                fontSize=18,
-                anchor='start',
-                color='gray'
-            ).configure_axisX(
-                labelLimit=0,
-                labelAngle=45 
-            )
-            st.altair_chart(chart_kecamatan, use_container_width=True)
-        else:
-            st.info("Tidak ada instansi dengan awalan 'Kecamatan' ditemukan dalam data.")
+        #Visualisasi Kecamatab
+        st.subheader("5 Kecamatan dengan Keluhan Masyarakat Terbanyak")
+        vis_kecamatan(uploaded_file)
+        
 
     except ValueError as ve:
         st.error(f"Error: {ve}")

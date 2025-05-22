fe672aa18adab37160f88b16ad054af73e3bed79
Jelang Ramadhan <144755170+JelangR@users.noreply.github.com>
2025-05-22 09:27:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -390,7 +390,7 @@ def top5Opd_keluhan_vis(data):
     height=300,
     title=f'Topik Keluhan - {instansi_terpilih}'
     ).configure_axisY(
-        labelLimit=8
+    labelLimit=8
     )
     st.altair_chart(chart, use_container_width=True)
 
